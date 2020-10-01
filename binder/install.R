09a89534ea7b7c4dda2e935814b0f1337fe1811b
C. Titus Brown <titus@idyll.org>
2020-10-01 06:18:30 -0700

    update install

--- binder/install.R
+++ binder/install.R
@@ -1,9 +1,9 @@
-pkgs = c("BiocManager", "ggplot2", "dplyr", "readr", "pheatmap", "kableExtra" ,"knitr", "rmarkdown")
+pkgs = c("BiocManager", "ggplot2", "dplyr", "readr", "pheatmap", "kableExtra" ,"knitr", "rmarkdown", "RSQLite", "data.table", "kableExtra")
 ncores = parallel::detectCores()
 install.packages(pkgs, Ncpus = ncores)
 
-if (!requireNamespace("BiocManager", quietly = TRUE))
- install.packages("BiocManager")
-
+install.packages("BiocManager")
 BiocManager::install("tximeta", version="3.10")
 BiocManager::install("DESeq2")
+BiocManager::install("SummarizedExperiment")
+
