187850b4603dc1d73059a8213bee89a44864fdd5
Saranya Canchi <36385305+s-canchi@users.noreply.github.com>
2021-06-16 14:24:18 -0700

    updated workshop commands file

--- workshop_commands.sh
+++ workshop_commands.sh
@@ -18,4 +18,20 @@ curl -L https://raw.githubusercontent.com/timflutre/trimmomatic/master/adapters/
 
 # Quality trim to remove adaptor from raw FASTQ file usin trimmomatic
 
-trimmomatic SE rnaseq/raw_data/ERR458493.fq.gz rnaseq/quality/ERR458493.qc.fq.gz ILLUMINACLIP:TRuSeq2-SE.fa:2:0:15 LEADING:2 TRAILING:2 SLIDINGWINDOW:4:2 MINLEN:25  
+trimmomatic SE rnaseq/raw_data/ERR458493.fq.gz rnaseq/quality/ERR458493.qc.fq.gz ILLUMINACLIP:TRuSeq2-SE.fa:2:0:15 LEADING:2 TRAILING:2 SLIDINGWINDOW:4:2 MINLEN:25
+
+# QC on trimmed file
+
+fastqc rnaseq/quality/ERR458493.qc.fq.gz --outdir rnaseq/quality/fastqc/ERR458493.qc_fastqc.html
+
+# Download and index the yeast transcriptome
+
+curl -L ftp://ftp.ensembl.org/pub/release-99/fasta/saccharomyces_cerevisiae/cdna/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa.gz -o rnaseq/reference/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa.gz
+
+# Generate salmon index
+
+salmon index --index rnaseq/quant/sc_ensembl_index --transcripts rnaseq/reference/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa.gz # --type quasi
+
+# Quantify reads with salmon
+
+salmon quant -i rnaseq/quant/sc_ensembl_index --libType A -r rnaseq/quality/ERR458493.qc.fq.gz -o rnaseq/quant/ERR458493_quant --seqBias --gcBias --validateMappings
